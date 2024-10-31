9d0796f9379e85fa94cdce90e7a539ab761d78b2
qkr10 <qkrtngks10@naver.com>
2024-10-31 11:45:19 +0900

    fix: test 실패 문제 해결
    @EnableJpaAuditing 을 메인 클래스에 바로 붙여줄 경우, 테스트 코드에 적용되지 않는 문제가 있음.
    Configuration 클래스에 옮겨 붙여서 해결

--- src/main/java/com/capstone/shop/ShopApplication.java
+++ src/main/java/com/capstone/shop/ShopApplication.java
@@ -4,11 +4,9 @@ import com.capstone.shop.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-@EnableJpaAuditing
 public class ShopApplication {
 
 	public static void main(String[] args) {
