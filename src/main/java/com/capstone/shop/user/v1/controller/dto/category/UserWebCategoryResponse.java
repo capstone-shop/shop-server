3f7fae04ec1d8767923a0716169932458fe40c60
qkr10 <qkrtngks10@naver.com>
2024-11-20 09:55:15 +0900

    fix: isLeaf 가 leaf 로 출력되는 현상 수정

--- src/main/java/com/capstone/shop/user/v1/controller/dto/category/UserWebCategoryResponse.java
+++ src/main/java/com/capstone/shop/user/v1/controller/dto/category/UserWebCategoryResponse.java
@@ -1,6 +1,7 @@
 package com.capstone.shop.user.v1.controller.dto.category;
 
 import com.capstone.shop.entity.Category;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,11 @@ public class UserWebCategoryResponse {
     private Long id;
     private String title;
     private boolean isLeaf;
+
+    @JsonProperty("isLeaf")
+    public boolean getIsLeaf() {
+        return isLeaf;
+    }
 
     public UserWebCategoryResponse(Category entity) {
         this.id = entity.getId();
