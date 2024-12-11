aea3c1a2128a3a88b97b000035ed18219caec7df
qkr10 <qkrtngks10@naver.com>
2024-12-12 00:27:44 +0900

    fix : "찜 목록", "등록한 상품 목록" 조회 api 응답에서 totalPage 값이 목록 갯수가 되는 버그 해결

--- src/main/java/com/capstone/shop/user/v1/controller/dto/merchandise/UserWebMerchandisePagination.java
+++ src/main/java/com/capstone/shop/user/v1/controller/dto/merchandise/UserWebMerchandisePagination.java
@@ -2,19 +2,17 @@ package com.capstone.shop.user.v1.controller.dto.merchandise;
 
 import com.capstone.shop.core.domain.entity.Merchandise;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
-@AllArgsConstructor
 @Getter
 public class UserWebMerchandisePagination {
-    private List<UserWebMerchandise> merchandise;
-    private Integer totalPage;
+    private final List<UserWebMerchandise> merchandise;
+    private final int totalPage;
 
-    public UserWebMerchandisePagination(List<Merchandise> entityList) {
+    public UserWebMerchandisePagination(List<Merchandise> entityList, int totalPage) {
         merchandise = UserWebMerchandise.entityListToDtoList(entityList);
-        totalPage = entityList.size();
+        this.totalPage = totalPage;
     }
 
     public UserWebMerchandisePagination(Page<Merchandise> entityPage) {
