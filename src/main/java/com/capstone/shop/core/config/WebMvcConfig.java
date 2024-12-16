9af42fe2ee3ee5f609633396756dbe9ea2ce224c
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-12-16 23:39:24 +0900

    fix : 채팅 timeline추가

--- src/main/java/com/capstone/shop/core/config/WebMvcConfig.java
+++ src/main/java/com/capstone/shop/core/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://api.induk.shop")
+                .allowedOrigins("https://induk.shop")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
