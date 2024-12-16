fb9bee825be1201da239adc01de76e8e8e6cd70f
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-12-16 20:13:12 +0900

    fix : 채팅 timeline추가

--- src/main/java/com/capstone/shop/core/domain/repository/MessageRepository.java
+++ src/main/java/com/capstone/shop/core/domain/repository/MessageRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByIdOrderByCreatedAtAsc(Long roomId);
+    List<Message> findByChatRoomIdOrderByCreatedAtAsc(Long roomId);
 
     Message findTopByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom);
 
